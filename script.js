16af9874627e93f1e65c5ad971d6bed886176b70
marlenakub77 <96764884+marlenakub77@users.noreply.github.com>
2022-01-21 12:32:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function reversedString(str) {
+
+let word = ('Akademia108');
+
+let string = word.split('');
+// console.log(string); 
+
+let reversedArray = string.reverse ('');
+// console.log(reversedArray)
+
+let joinArray = string.join ('');
+console.log(joinArray);
+
+}
+reversedString ()
